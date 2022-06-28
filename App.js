ad675521d95f5cbc8743ee262430ee3dfa49f3f1
Justin Huang <justinhuangthedragon@gmail.com>
2022-06-28 02:12:29 -0700

    Final_Project

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
+import React from "react";
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator} from 'react-native';
+import { Button} from 'react-native';
+import { FlatList} from 'react-native';
+import { Image} from 'react-native';
+import { ImageBackground} from 'react-native';
+import { KeyboardAvoidingView} from 'react-native';
+import { Modal} from 'react-native';
+import { RefreshControl} from 'react-native';
+import { ScrollView} from 'react-native';
+import { SectionList} from 'react-native';
+import { StatusBar} from 'react-native';
+import { StyleSheet} from 'react-native';
+import { Text} from 'react-native';
+import { TextInput} from 'react-native';
+import { TouchableHighlight} from 'react-native';
+import { TouchableOpacity} from 'react-native';
+import { TouchableWithoutFeedback} from 'react-native';
+import { View} from 'react-native';
+import { VirtualizedList} from 'react-native';
+import { DrawerLayoutAndroid} from 'react-native';
+import { TouchableNativeFeedback} from 'react-native';
+import { InputAccessoryView} from 'react-native';
+import { SafeAreaView} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
